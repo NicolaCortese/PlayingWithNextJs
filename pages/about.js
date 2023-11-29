f323810b0ac79783f5090cf1ca1f7bdb955e452f
nicola.cortese <ncortese@deloitte.co.uk>
2023-11-29 08:52:01 +0000

    initial commit

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+export default function about() {
+  return (
+    <div>
+      <h1>about</h1>
+      <p>This is an app about lala</p>
+      <p>Version 1.0.0</p>
+    </div>
+  );
+}
---
+++ pages/events/[slug].js
@@ -0,0 +1,5 @@
+export default function EventPage() {
+  (
+    <div>Event Page</div>
+  )
+}
